d7aeaffc516371218a0d70a4ac43c625b726b936
yuzhenqing <yuzq@0791jx.cn>
2020-10-27 20:48:49 +0800

    添加vue-amap

---
+++ src/components/demo/demo.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="amp-demo">
+        <el-amap vid="amap" :zoom="zoom" :center="center" class="amap-demo" v-if="reFresh">
+            <!-- 点坐标  -->
+            <el-amap-marker :position="marker.position" :visible="marker.visible" :draggable="marker.draggable" vid="index" ></el-amap-marker>
+            <el-amap-marker :position="marker2.position" :visible="marker2.visible" :draggable="marker2.draggable" vid="index2"></el-amap-marker>
+            <!-- 标记文本 -->
+            <el-amap-text v-for="(text,index) in texts" :key="index" :text="text.text" :offset="text.offset" :position="text.position" :events="text.events"></el-amap-text>
+            <!-- 折线路径 -->
+            <el-amap-polyline :editable="polyline.editable"  :path="polyline.path" ></el-amap-polyline>
+        </el-amap>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'demo',
+        data() {
+            return {
+                reFresh:true,
+                slotStyle: {
+                    padding: '2px 8px',
+                    background: '#eee',
+                    color: '#333',
+                    border: '1px solid #aaa'
+                },
+                zoom: 12,
+                center: [121.5273285, 31.25515044],
+                polyline: {
+                    path: [[121.5389385, 31.21515044], [121.5273285, 31.21515044]],
+                    editable: false
+                },
+                marker: {
+                    position: [121.5273285, 31.21515044],
+                    visible: true,
+                    draggable: false,
+                    content:'2',
+                    template: '<span>2</span>',
+                },
+                marker2: {
+                    position: [121.5273285, 31.21515044],
+                    visible: true,
+                    draggable: false,
+                    content:'1',
+                    template: '<span>1</span>',
+                },
+                texts: [
+                    {
+                        position: [121.5273285, 31.21515044],
+                        text: '收货地',
+                        offset: [0, -70],
+                    },
+                    {
+                        position: [121.5273285, 31.21515044],
+                        text: '发货地',
+                        offset: [0, -70],
+                    },
+                ],
+            }
+        },
+        methods: {
+            menuTree(){
+                this.reFresh= false
+                this.$nextTick(()=>{
+                    this.reFresh = true
+                })
+            },
+        }
+    };
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueAMap from "vue-amap";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+//main.js引入
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: 'd6a13046ccbbe124fda43844ffd7f21f',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  v: '1.4.4'
+});
 
 new Vue({
   render: h => h(App),
