0c3837e92539fa1298e1c530f4665766fbdcbfd7
yuzhenqing <yuzq@0791jx.cn>
2020-11-02 19:13:09 +0800

    添加

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ Vue.config.productionTip = false;
 //main.js引入
 Vue.use(VueAMap);
 VueAMap.initAMapApiLoader({
-  key: 'd6a13046ccbbe124fda43844ffd7f21f',
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geocoder', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  key: 'd6a13046ccbbe124fda43844ffd7f21f', //, 'AMap.Geolocation'
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geolocation', 'AMap.Geocoder', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
   v: '1.4.4',
   uiVersion: '1.0.11' // 版本号
 });
